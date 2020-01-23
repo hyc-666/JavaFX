f3f765df8eeedc993221354e3d99b23d60593666
hyc_666 <2213671872@qq.com>
2020-01-24 02:48:25 +0800

    Hello word以及窗口属性

--- JavaFX_01/src/JavaFX_01/Hello.java
+++ JavaFX_01/src/JavaFX_01/Hello.java
@@ -11,7 +11,19 @@ public class Hello extends Application{
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        primaryStage.setTitle("Hello world");
-        primaryStage.show();
+        primaryStage.setTitle("Hello world");//设置标题
+
+        primaryStage.show();//让窗口显示出来
+        primaryStage.setMaxWidth(800);//设置最大宽度
+        primaryStage.setMaxHeight(800);//设置最大高度
+
+        primaryStage.setMinWidth(300);//设置窗口最小宽度
+        primaryStage.setMinHeight(300);//设置窗口最小高度
+
+        primaryStage.setWidth(500);//设置默认宽度
+        primaryStage.setHeight(500);//设置默认的高度
+
+        primaryStage.setFullScreen(true);//全屏
+        //primaryStage.setIconified(true);//最小化
     }
 }
