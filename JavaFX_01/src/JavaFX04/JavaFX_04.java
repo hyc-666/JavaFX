3ad5ff24efea36aa068f1810a8ced49323ff0463
hyc_666 <2213671872@qq.com>
2020-01-28 00:25:27 +0800

    JavaFX_04窗口属性

--- JavaFX_01/src/JavaFX04/JavaFX_04.java
+++ JavaFX_01/src/JavaFX04/JavaFX_04.java
@@ -1,29 +1,38 @@
 package JavaFX04;
 
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
-import java.awt.*;
-
-public class JavaFX_04 extends Application {
+public class JavaFX_04 extends Application{
     public static void main(String[] args) {
         launch(args);
     }
-    public void start(Stage stage){
-        Stage s1 = new Stage();
-        s1.setTitle("s1");
 
-        Stage s2 = new Stage();
-        s2.setTitle("s2");
-        s2.initOwner(s1);//s1与s2关联
-        s2.initModality(Modality.WINDOW_MODAL);//只能先操作s2
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setHeight(500);
+        primaryStage.setWidth(800);
 
-        Stage s3 = new Stage();
-        s3.setTitle("s3");
+        //primaryStage.setAlwaysOnTop(true);//设置窗口置顶
+        //primaryStage.setOpacity(0.5);//窗口透明度
 
-        s1.show();
-        s2.show();
-        s3.show();
+        primaryStage.xProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+                System.out.println("窗口X坐标位置："+ newValue);
+            }
+        });
+        primaryStage.yProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+                System.out.println("窗口Y坐标位置：" + newValue);
+            }
+        });
+
+
+        primaryStage.show();
     }
 }
